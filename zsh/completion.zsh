7a33f95441032c7419c931ffc7780971082b6b87
Fabian Franke <mail@fabianfranke.de>
2012-07-18 10:13:25 +0200

    [add]: host completion for mosh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -47,9 +47,11 @@ zstyle ':completion:*:*:(^rm):*:*files' ignored-patterns '*?.o' '*?.c~' \
 # ignore completion functions (until the _ignored completer)
 zstyle ':completion:*:functions' ignored-patterns '_*'
 
-#local _myhosts
+# local _myhosts
 _myhosts=( ${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[0-9]*}%%\ *}%%,*} )
 zstyle ':completion:*' hosts $_myhosts
+# complete hosts for mosh too 
+compdef mosh=ssh
 
 # complete word from history with menu (from Book: ZSH, OpenSource-Press)
 zle -C hist-complete complete-word _generic
