7ff4a06b44a9db06acd7ed9ba7f886ed6d2301ee
Fabian Franke <mail@fabianfranke.de>
2013-04-22 12:34:13 +0200

    [add] auto escape pasted URLs

--- zsh/correction.zsh
+++ zsh/correction.zsh
@@ -6,3 +6,7 @@ alias mv='nocorrect mv'
 alias cp='nocorrect cp'
 alias mysql='nocorrect mysql'
 alias mkdir='nocorrect mkdir'
+
+# quote pasted URLs
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
