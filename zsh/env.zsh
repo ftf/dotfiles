dec3d60634f10f007a8fd9d6731813e870e3a557
Fabian Franke <mail@fabianfranke.de>
2013-08-07 15:23:28 +0200

    [add] python3.2 path added

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -16,7 +16,7 @@ fi
 
 export PATH=/usr/local/sbin:/usr/local/bin:~/.dotfiles/bin:$PATH
 #export PYTHONPATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages:/System/Library/Frameworks/Python.framework/Versions/Current
+export PYTHONPATH=/usr/local/lib/python3.3/site-packages:/usr/local/lib/python2.7/site-packages:/System/Library/Frameworks/Python.framework/Versions/Current
 export PYTHONSTARTUP=~/.pythonrc
 
 # for gpg curses gui
