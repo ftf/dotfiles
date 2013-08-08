5ac7f3598ad9721aadab8e1f6b29500a07135e46
Fabian Franke <mail@fabianfranke.de>
2013-08-08 15:55:01 +0200

    [change] use hist_ignore_all_dups instead of hist_ignore_dups

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,13 +4,15 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_all_dups # ignore duplication command history list
 setopt share_history # share command history data
 
 setopt hist_verify
 setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first
+
+# do not save cmd to history if prefixed with a space
 setopt hist_ignore_space
 
 setopt append_history
