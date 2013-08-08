df05dd950bdb6c970fd907e2490f86e184a061b2
Fabian Franke <mail@fabianfranke.de>
2013-08-08 16:35:13 +0200

    [change] smaller output, different symbols

--- bin/battery.py
+++ bin/battery.py
@@ -17,20 +17,20 @@ charge_threshold = int(math.ceil(10 * charge))
 
 # Output
 
-total_slots, slots = 10, []
-filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
-empty = (total_slots - len(filled)) * u'▹'
+total_slots, slots = 5, []
+filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'•'
+empty = (total_slots - len(filled)) * u'·'
 
 out = (filled + empty).encode('utf-8')
 import sys
 
 color_green = '%{[32m%}'
-color_yellow = '%{[1;33m%}'
-color_red = '%{[31m%}'
+color_yellow = '%{[1;33m%}'
+color_red = '{[31m%}'
 color_reset = '%{[00m%}'
 color_out = (
-    color_green if len(filled) > 6
-    else color_yellow if len(filled) > 4
+    color_green if len(filled) > 3 
+    else color_yellow if len(filled) > 2
     else color_red
 )
 
