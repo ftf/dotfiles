8ce36cdd7e3e77574b325240dd851c48dd2e9a3b
Fabian Franke <mail@fabianfranke.de>
2014-09-04 15:10:59 +0200

    [fix] $PATH modified, ~/.dotfiles/bin

--- bash/env.bash
+++ bash/env.bash
@@ -3,7 +3,6 @@ export GPG_TTY=`tty`
 GREP_COLOR="1;31"
 GREP_OPTIONS="-i --color=auto"
 CLICOLOR=1
-export PATH=/usr/local/sbin:/usr/local/bin:~/.dotfiles/bin:$PATH
 if [[ -a  /usr/local/opt/curl-ca-bundle/share/ca-bundle.crt ]]; then
   export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 fi
